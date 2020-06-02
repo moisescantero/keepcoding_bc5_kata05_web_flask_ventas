b18c3d85791d1b0347f910e2b99ac38cc0515532
moises cantero lopez <moisescantero@gmail.com>
2020-06-02 22:17:14 +0200

    Validación formulario mostrando errores

--- the_app/__init__.py
+++ the_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)#creamos app y configuramos instancia relativa al fichero config.py
-app.config.from_object("config")#ruta al fichero config.py donde guardamos nuestra SECRET_KEY para evitar phising a nuestra web
+app.config.from_object("_config")#ruta al fichero config.py donde guardamos nuestra SECRET_KEY para evitar phising a nuestra web
 
 
 from the_app import routes
--- the_app/forms.py
+++ the_app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 class ProductForm(FlaskForm):
     tipo_producto = StringField('Tipo de producto', validators=[DataRequired(), Length(min=3, message="Debe tener al menos 3 caracteres.")])
-    precio_unitario = FloatField('Precio Unitario', validators=[DataRequired()])
+    precio_unitario = FloatField('Precio Unitario', validators=[DataRequired(message="Introduce algo, nano")])
     coste_unitario = FloatField('Coste Unitario', validators=[DataRequired()])
 
     submit = SubmitField('Aceptar')
